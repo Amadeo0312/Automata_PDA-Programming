032520487079c03b741275076e9e4bf9ee283315
Louisse Amadeo Romero <louisseamadeo.romero@cit.edu>
2022-11-29 02:37:34 +0800

    Add Program Class

--- Automata_PDA-Programming/PushDown_Automata/PushDown_Automata/Program.cs
+++ Automata_PDA-Programming/PushDown_Automata/PushDown_Automata/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PushDown_Automata;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Problems: ");
+        Console.WriteLine("[1] - Palindromes");
+        Console.WriteLine("[2] - Balanced Symbols");
+
+        Console.Write("\nEnter a Problem Number: ");
+        int prob = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("\nEnter A String: ");
+        var str = Console.ReadLine();
+
+        Console.WriteLine(" ");
+
+        switch(prob)
+        {
+           case 1:
+                Palindromes p = new Palindromes();
+                bool check = p.IsPalindrome(str);
+                if (check == true)
+                    Console.WriteLine("The Input \"" + str + "\" is Accepted");
+                else
+                    Console.WriteLine("The Input \"" + str + "\" is not Accepted");
+                break;
+           case 2:
+                BalancedSymbols b = new BalancedSymbols();
+                check = b.IsBalanced(str);
+                if (check == true)
+                    Console.WriteLine("The Input \"" + str + "\" is Accepted");
+                else
+                    Console.WriteLine("The Input \"" + str + "\" is not Accepted");
+                break;
+        }
+    }
+}
