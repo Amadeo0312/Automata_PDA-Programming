0f0b4ee2a882c8f6eaf1a61d2ff5e13fec2ec80a
Louisse Amadeo Romero <louisseamadeo.romero@cit.edu>
2022-11-29 01:58:29 +0800

    Add Palindrome

---
+++ Automata_PDA-Programming/PushDown_Automata/PushDown_Automata/Palindromes.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+
+namespace PushDown_Automata
+{
+    public class Palindromes
+    {
+        public bool IsPalindrome(string s)
+        {
+            string str = s;
+            char[] word = str.ToCharArray();
+            int length = word.Length;
+            
+            Stack stack = new Stack();
+
+            for(int i = 0; i < length - (length/2); i++)
+            {   
+                if(i == length/2)
+                {
+                    Console.WriteLine("Skip one " + word[i]);
+                    Display(stack);
+                }
+                else
+                {
+                    Console.WriteLine("Push " + word[i]);
+                    stack.Push(word[i]);
+                    Display(stack);
+                }
+            }
+
+            for(int i = length - (length/2); i < length; i++)
+            {
+                if (word[i] == Convert.ToChar(stack.Peek()))
+                {
+                    Console.WriteLine("pop " + stack.Peek());
+                    stack.Pop();
+                    Display(stack);
+                }
+                else
+                {
+                    Console.WriteLine("Pop failed. Not a Match");
+                    Display(stack);
+                }
+            }
+
+
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;    
+        }
+
+        public void Display(Stack stack)
+        {
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Current Stack: Empty");
+            }
+            else
+            {
+                Console.Write("Current Stack: ");
+                foreach (var item in stack)
+                {
+                    Console.Write(item + " <- ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
